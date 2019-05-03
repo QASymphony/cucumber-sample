e5862be23198c86be6359dfd405422cbbcd06a9f
Huy Nguyen <huynguyen@qasymphony.com>
2019-05-03 10:34:53 -0400

    Update TestUtil.java
    Attempt to fix compilation error #2

--- src/test/java/com/utils/TestUtil.java
+++ src/test/java/com/utils/TestUtil.java
@@ -2,8 +2,8 @@ package com.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
